ac1fcb147a1424ec3f0909fa5a81cda989fd9148
Will Hedges <wshedges@outlook.com>
2022-12-15 11:24:20 -0600

    added request form html to RequestForm.js

--- src/scripts/RequestForm.js
+++ src/scripts/RequestForm.js
@@ -5,18 +5,29 @@
 */
 
 document.addEventListener("click", (clickEvt) => {
-  if (clickEvt.target.id === "submitRequest") {
-  }
+    if (clickEvt.target.id === "submitRequest") {
+    }
 });
 
 export const RequestForm = () => {
-  let html = `
+    let html = `
     <div class="field flex column">
-     <label class="label" for="name">Name</label>
-     <input type="text" id="name" class="input">
 
-     <button class="button" id="submitRequest">Submit Request</button>
+      <label class="label" for="name">Name</label>
+      <input type="text" id="name" class="input">
+
+      <label class="label" for="purpose">Purpose</label>
+      <input type="text" id="purpose" class="input">
+
+      <label class="label" for="type">Type</label>
+      <select id="type" name="type">
+        <option value="">Select a Type</option>
+        <option value="1">Potion</option>
+        <option value="2">Elixir</option>
+      </select>
+
+      <button class="button" id="submitRequest">Submit Request</button>
     </div>
     `;
-  return html;
+    return html;
 };
